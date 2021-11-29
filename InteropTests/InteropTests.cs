5bb22388b136e29243e1f1ff57058ef1b2b813b8
100029962 <joger.hansegard@ge.com>
2021-11-29 19:44:29 +0100

    Add test project for interop tests with .NET. Also disable Win32 configuration, so I don't have to maintain it

---
+++ InteropTests/InteropTests.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace InteropTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
