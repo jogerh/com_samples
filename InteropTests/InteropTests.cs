50397b156410e58383c3f3138d54ded06d6783a7
100029962 <joger.hansegard@ge.com>
2021-11-29 20:26:37 +0100

    Add test that demonstrates that .NET code can call COM interfaces directly by using an interop assembly generated from the type library.

--- InteropTests/InteropTests.cs
+++ InteropTests/InteropTests.cs
@@ -1,18 +1,30 @@
+using System;
+using System.Diagnostics;
 using NUnit.Framework;
-
+using Moq;
+using InterfacesInterop;
 namespace InteropTests
 {
-    public class Tests
+    public class InteropTestsWithHens
     {
         [SetUp]
         public void Setup()
         {
+            Type comServerType = Type.GetTypeFromProgID("AtlHenLib.AtlHen.1");
+            Assert.NotNull(comServerType);
+            m_hen = Activator.CreateInstance(comServerType) as IHen;
         }
 
         [Test]
-        public void Test1()
+        public void RequireThat_CluckObserverIsCalled_WhenPassingAsMockToAtlHen()
         {
-            Assert.Pass();
+            var cluckObserver = new Mock<IAsyncCluckObserver>();
+
+            m_hen.CluckAsync(cluckObserver.Object); // TODO: Verify that Cluck is called on the right thread
+
+            cluckObserver.Verify(mock => mock.OnCluck(), Times.AtLeastOnce);
         }
+
+        private IHen m_hen;
     }
 }
