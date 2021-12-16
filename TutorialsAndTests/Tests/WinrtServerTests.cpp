51993d6785b3e150b3e32a1db4229854675947fd
Fredrik Orderud <fredrik.orderud@ge.com>
2021-12-16 12:19:24 +0100

    WinrtServer: Add example of how to use structs
    Introduce a Pos3 struct for 3D coordinates that is passed as both input & output argument to a new Add method.
    
    NOTICE: Structs members are _not_ zero-initialized by default, so it's good practice to zero-initialize them explicitly before usage to avoid "random" state in release builds.

--- TutorialsAndTests/Tests/WinrtServerTests.cpp
+++ TutorialsAndTests/Tests/WinrtServerTests.cpp
@@ -42,3 +42,22 @@ TEST(WinrtServerTests, RequireThat_GivingProgrammerCoffee_IncreasesMotviation)
 
     EXPECT_TRUE(motivationAfterCoffee > motivationBeforeCoffee);
 }
+
+TEST(WinrtServerTests, RequireThat_ProgrammerCanAdd3dCoordinates)
+{
+    // Initialize the Windows Runtime.
+    RoInitializeWrapper initialize(RO_INIT_SINGLETHREADED);
+    HR(initialize);
+
+    ComPtr<ABI::WinrtServer::IProgrammer> programmer;
+    HR(ActivateInstance(HStringReference(L"WinrtServer.Programmer").Get(), programmer.GetAddressOf()));
+
+    ABI::WinrtServer::Pos3 a = { 1, 2, 3 };
+    ABI::WinrtServer::Pos3 b = { 1, 2, 3 };
+    ABI::WinrtServer::Pos3 sum = {}; // zero-initialize
+    HR(programmer->Add(a, b, &sum));
+
+    EXPECT_EQ(sum.x, a.x + b.x);
+    EXPECT_EQ(sum.y, a.y + b.y);
+    EXPECT_EQ(sum.z, a.z + b.z);
+}
