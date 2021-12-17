00a55707aff097d6913b6a72cd06679a23c45199
100029962 <joger.hansegard@ge.com>
2021-12-17 12:27:55 +0100

    Instead of copying generated files from WinrtServer, we can instead use a project reference.
    When compiling the TutorialsAndTests.vcxproj project, it now generates the necessary files into
    
        Build\Intermediate\TutorialsAndTests\Generated Files\winrt
    
    I think this approach will scale better if we have more than one winrt component in the solution.
    
    If the WinrtServer should be consumed by other projects than the projects in this solution, I think those projects should generate their own files using cppwinrt.exe.
    
    See also https://docs.microsoft.com/en-us/windows/uwp/winrt-components/create-a-windows-runtime-component-in-cppwinrt

--- WinrtServer/CreateCppInterop.bat
+++
@@ -1,9 +0,0 @@
-set OutDir=%1
-
-if exist %OutDir%Include\winrt\ (
-    del /s /q %OutDir%Include\winrt\ || exit /b 1
-)
-
-REM Copy the winrt folder. 
-xcopy /E /I "Generated Files\winrt\base.h" %OutDir%Include\winrt\ || exit /b 1
-xcopy /E /I "Generated Files\winrt\WinrtServer*" %OutDir%Include\winrt\ || exit /b 1
