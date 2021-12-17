12b71867dd46c423d2b9e48880f287fb51fc22ff
supersega <malyshev13091995@gmail.com>
2021-12-17 11:52:31 +0200

    Install buildenv to Build folder to make less noise

--- PyComServer/RegPyComServer.bat
+++ PyComServer/RegPyComServer.bat
@@ -1,10 +1,11 @@
 setlocal
 pushd %~dp0
 
-py -3 -m venv buildenv || exit /b 1
-call buildenv\Scripts\Activate.bat || exit /b 1
-
 @echo on
+
+py -3 -m venv ..\Build\buildenv || exit /b 1
+call ..\Build\buildenv\Scripts\Activate.bat || exit /b 1
+
 python -m pip install -r requirements.txt || exit /b 1
 
 :: In C++ projects we do it automatically, but for python server handcrafted generation needed 
