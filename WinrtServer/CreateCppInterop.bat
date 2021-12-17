ca12f15c549862d5e15d1f1a43cfdfbb2a232f46
Fredrik Orderud <fredrik.orderud@ge.com>
2021-12-17 10:17:43 +0100

    WinrtServer: Also copy missing base.h
    Transitive dependency by WinrtServer.h (include in line number 6).

--- WinrtServer/CreateCppInterop.bat
+++ WinrtServer/CreateCppInterop.bat
@@ -5,4 +5,5 @@ if exist %OutDir%Include\winrt\ (
 )
 
 REM Copy the winrt folder. 
+xcopy /E /I "Generated Files\winrt\base.h" %OutDir%Include\winrt\ || exit /b 1
 xcopy /E /I "Generated Files\winrt\WinrtServer*" %OutDir%Include\winrt\ || exit /b 1
